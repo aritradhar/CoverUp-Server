51bf3c9a1941053696e13b8bc301c324d1b12a10
dhara <dhara@057f481d-c402-db11-8d0d-0014225fb3b7>
2016-05-23 08:11:26 +0000

    added unique IP address stats

--- src/com/ethz/server/Stats.java
+++ src/com/ethz/server/Stats.java
@@ -1,10 +1,15 @@
 package com.ethz.server;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Stats {
 	
 	public static long TOTAL_CONNECTIONS = 0;
 	public static long LIVE_CONNECTIONS = 0;
 	
 	public static boolean keygen_done = false;
+	
+	public static Set<String> UNIQUE_IP_ADDRESSES = new HashSet<>();
 
 }
