f5e889dbcd6fa9f6c19fecf4658252ef2309d1d5
dhara <dhara@057f481d-c402-db11-8d0d-0014225fb3b7>
2016-07-08 10:48:09 +0000

    Generalized for windows and linux

--- aritra/UndergroundServer/src/com/ethz/ugs/test/Test.java
+++ aritra/UndergroundServer/src/com/ethz/ugs/test/Test.java
@@ -23,42 +23,16 @@ public class Test
 
 	public static void main(String[] args) throws IOException, NoSuchAlgorithmException, NoSuchProviderException {
 
-		boolean exists = SiteMap.loadTable();
+		SiteMap.loadTable();
 
-		if(System.getProperty("os.name").contains("Windows"))
+		File[] files = new File(ENV.SOURCE_DOCUMENT_LOCATION).listFiles();
+
+		for(File file : files)
 		{
-			if(!exists)
-			{
-				FountainTableRow row1 = new FountainTableRow(ENV.SOURCE_DOCUMENT_LOCATION, 10000, 50);
-				row1.makeDroplets();
-			
-			//System.out.println(row1.toString());
-
-				SiteMap.insertRowToTable("C:\\1.txt", row1);
-			}
-			else
-				System.out.println("Already exists in table. Skipped...");
-			
+			FountainTableRow row = new FountainTableRow(file.getAbsolutePath(), 10000, 50);
+			row.makeDroplets();
+			SiteMap.insertRowToTable(file.getAbsolutePath(), row);
 		}
-		else
-		{
-			File[] files = new File(ENV.SOURCE_DOCUMENT_LOCATION).listFiles();
-
-			for(File file : files)
-			{
-				if(!exists)
-				{
-					FountainTableRow row = new FountainTableRow(file.getAbsolutePath(), 10000, 50);
-					row.makeDroplets();
-
-					SiteMap.insertRowToTable(file.getAbsolutePath(), row);
-				}
-				else
-					System.out.println("Already exists in table. Skipped...");
-			}
-
-		}
-
 
 		SiteMap.saveTable();
 
