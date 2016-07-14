8930fb5435d11370969b35be6144886afcd65ff5
dhara <dhara@057f481d-c402-db11-8d0d-0014225fb3b7>
2016-07-14 16:46:19 +0000

    update

--- aritra/UndergroundServer/src/com/ethz/ugs/server/ServerUtil.java
+++ aritra/UndergroundServer/src/com/ethz/ugs/server/ServerUtil.java
@@ -55,21 +55,34 @@ public class ServerUtil {
 		StringBuilder stringBuilder = new StringBuilder();
 		BufferedReader bufferedReader = null;
 
-		try {
+		try 
+		{
 			InputStream inputStream = request.getInputStream();
-			if (inputStream != null) {
+			
+			if (inputStream != null) 
+			{
 				bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
 				char[] charBuffer = new char[128];
 				int bytesRead = -1;
-				while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+				
+				while ((bytesRead = bufferedReader.read(charBuffer)) > 0) 
+				{
 					stringBuilder.append(charBuffer, 0, bytesRead);
 				}
-			} else {
-				stringBuilder.append("");
+			} 
+			else 
+			{
+				stringBuilder.append("");	
 			}
-		} catch (IOException ex) {
+			
+			inputStream.close();
+		} 
+		catch (IOException ex)
+		{
 			throw ex;
-		} finally {
+		} 
+		finally 
+		{
 			if (bufferedReader != null) {
 				try {
 					bufferedReader.close();
