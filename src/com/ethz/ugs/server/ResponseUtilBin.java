285c928c5cf9818560e97e50494952d915145e0f
dhara <dhara@057f481d-c402-db11-8d0d-0014225fb3b7>
2016-09-27 13:32:16 +0000

    added slice table as tree structure

--- src/com/ethz/ugs/server/ResponseUtilBin.java
+++ src/com/ethz/ugs/server/ResponseUtilBin.java
@@ -91,7 +91,7 @@ public class ResponseUtilBin {
 		byte[] packetToSend = new byte[ENV.FIXED_PACKET_SIZE_BIN];
 		byte[] tableLenBytes = ByteBuffer.allocate(Integer.BYTES).putInt(theTableBytes.length).array();
 		
-		String sliceTable = InitialGen.sdm.getSliceTableAsJson();
+		String sliceTable = InitialGen.sdm.getSliceTableAsJsonTree();
 		byte[] sliceTableBytes = sliceTable.getBytes(StandardCharsets.UTF_8);
 		byte[] sliceTableLenBytes = ByteBuffer.allocate(Integer.BYTES).putInt(sliceTableBytes.length).array();
 		
