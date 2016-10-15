318f9d4e81faa5d95ab9502f1e4c13388c5c7b0d
dhara <dhara@057f481d-c402-db11-8d0d-0014225fb3b7>
2016-10-15 23:30:07 +0000

    Update chat response + add ssl id for chat management

--- src/com/ethz/ugs/server/ResponseUtilBinConstantTimeChat.java
+++ src/com/ethz/ugs/server/ResponseUtilBinConstantTimeChat.java
@@ -76,7 +76,7 @@ public class ResponseUtilBinConstantTimeChat {
 		long start = System.nanoTime(), end = 0;
 
 		long additionalDelay = (long) ((Math.abs(Math.round(rand.nextGaussian() * 3 + 12))) * Math.pow(10, 6));
-				
+
 		OutputStream out = response.getOutputStream();
 		//garbage
 		if( Math.random() <= ENV.PROB_THRESHOLD )
@@ -92,7 +92,7 @@ public class ResponseUtilBinConstantTimeChat {
 					//send garbage in this case
 					byte[] garbageReturn = new byte[ENV.FIXED_PACKET_SIZE_BIN];
 					rand.nextBytes(garbageReturn);
-					
+
 					//additional delay start
 					long offset = additionalDelay + ENV.FIXED_REQUEST_PROCESSING_TIME_NANO - (System.nanoTime() - start);
 					try {
@@ -102,7 +102,7 @@ public class ResponseUtilBinConstantTimeChat {
 					}
 					end = System.nanoTime();
 					//additional delay end
-					
+
 					out.write(garbageReturn);
 				}
 				//chat data :D
@@ -119,13 +119,13 @@ public class ResponseUtilBinConstantTimeChat {
 					//additional delay end					
 					out.write(toSend);
 				}
-				
+
 			}
 			else
 			{
 				byte[] garbageReturn = new byte[ENV.FIXED_PACKET_SIZE_BIN];
 				rand.nextBytes(garbageReturn);	
-				
+
 				//additional delay start
 				long offset = additionalDelay + ENV.FIXED_REQUEST_PROCESSING_TIME_NANO - (System.nanoTime() - start);
 				try {
@@ -135,10 +135,10 @@ public class ResponseUtilBinConstantTimeChat {
 				}
 				end = System.nanoTime();
 				//additional delay end
-				
+
 				out.write(garbageReturn);
 			}
-			
+
 			MainServer.logger.info("Droplet noInt garbage : " + (end - start)  + " ns");
 			out.flush();
 			out.close();
@@ -166,51 +166,53 @@ public class ResponseUtilBinConstantTimeChat {
 			response.flushBuffer();
 		}
 	}
-		
+
 	/**
 	 * Serves chat data
 	 * @param request
 	 * @param response
 	 * @param postBody
+	 * @throws IOException 
 	 */
-	public static void dropletPleaseChatBin(HttpServletRequest request, HttpServletResponse response, byte[] postBody) 
+	public static void dropletPleaseChatBin(HttpServletRequest request, HttpServletResponse response, byte[] postBody) throws IOException 
 	{
 		long start = System.nanoTime(), end = 0;
 		long additionalDelay = (long) ((Math.abs(Math.round(rand.nextGaussian() * 3 + 12))) * Math.pow(10, 6));
+		String sslId = (String) request.getAttribute("javax.servlet.request.ssl_session_id");
 
-		
+		OutputStream out = response.getOutputStream();
 		//0x00/0x01 (1) | reserved (3) | p1 | p2 | ...
 		//<p_i = sr//R_adder(8) | S_addr(8) | iv(16) | len(4) | enc_Data(n) | sig(64) (on 0|1|2|3|4)
-		
+
 		int pointer = 4;
-		
+
 		while(true)
 		{
 			//reached to the end
 			if(pointer >= postBody.length)
 				break;
-			
+
 			try
 			{
-			//get the target address for storing in the chat manager class
-			byte[] targetAddressBytes = new byte[8];
-			System.arraycopy(postBody, 4, targetAddressBytes, 0, 8);
-			
-			int newPointer = pointer + 16; //traverse source and dest address
-			byte[] datalenBytes = new byte[4]; //data len
-			System.arraycopy(postBody, newPointer, datalenBytes, 0, 4);
-			int datalen = ByteBuffer.wrap(datalenBytes).getInt();
-			newPointer += 4; //add offset for datalen
-			
-			newPointer += datalen; //add offset for data length
-			newPointer += 64; //add offset for ed25519 signature length
-			
-			byte[] dataChunk = new byte[newPointer - pointer + 1];
-			System.arraycopy(postBody, pointer, dataChunk, 0, dataChunk.length);
-			
-			MainServer.chatManager.addChat(Base64.getUrlEncoder().encodeToString(targetAddressBytes), dataChunk);		
-			
-			pointer = newPointer;
+				//get the target address for storing in the chat manager class
+				byte[] targetAddressBytes = new byte[8];
+				System.arraycopy(postBody, 4, targetAddressBytes, 0, 8);
+
+				int newPointer = pointer + 16; //traverse source and dest address
+				byte[] datalenBytes = new byte[4]; //data len
+				System.arraycopy(postBody, newPointer, datalenBytes, 0, 4);
+				int datalen = ByteBuffer.wrap(datalenBytes).getInt();
+				newPointer += 4; //add offset for datalen
+
+				newPointer += datalen; //add offset for data length
+				newPointer += 64; //add offset for ed25519 signature length
+
+				byte[] dataChunk = new byte[newPointer - pointer + 1];
+				System.arraycopy(postBody, pointer, dataChunk, 0, dataChunk.length);
+
+				MainServer.chatManager.addChat(sslId, Base64.getUrlEncoder().encodeToString(targetAddressBytes), dataChunk);		
+
+				pointer = newPointer;
 			}
 			catch(Exception ex)
 			{
@@ -218,8 +220,44 @@ public class ResponseUtilBinConstantTimeChat {
 				ex.printStackTrace(System.out);
 			}
 		}
-		
-		
+		//send chat data here
+
+		byte[] toSend = MainServer.chatManager.getChat(sslId);
+
+		if(toSend == null)	
+		{
+			//send garbage in this case
+			byte[] garbageReturn = new byte[ENV.FIXED_PACKET_SIZE_BIN];
+			rand.nextBytes(garbageReturn);
+
+			//additional delay start
+			long offset = additionalDelay + ENV.FIXED_REQUEST_PROCESSING_TIME_NANO - (System.nanoTime() - start);
+			try {
+				TimeUnit.NANOSECONDS.sleep(offset);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			end = System.nanoTime();
+			//additional delay end
+
+			out.write(garbageReturn);
+		}
+		//chat data :D
+		else	
+		{			
+			//additional delay start
+			long offset = additionalDelay + ENV.FIXED_REQUEST_PROCESSING_TIME_NANO - (System.nanoTime() - start);
+			try {
+				TimeUnit.NANOSECONDS.sleep(offset);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			end = System.nanoTime();
+			//additional delay end					
+			out.write(toSend);
+		}
+
+
 		//additional delay start
 		long offset = additionalDelay + ENV.FIXED_REQUEST_PROCESSING_TIME_NANO - (System.nanoTime() - start);
 		try {
@@ -228,13 +266,13 @@ public class ResponseUtilBinConstantTimeChat {
 			e.printStackTrace();
 		}
 		end = System.nanoTime();
-		
+
 		MainServer.logger.info("Chat packet : " + (end - start)  + " ns");
 		//additional delay end
-		
+
 	}
-	
-	
+
+
 	/**
 	 * Make the chat data packet
 	 * packet len (4) | seedlen (4) ->0 | Magic (8) | Data | Padding
@@ -245,13 +283,13 @@ public class ResponseUtilBinConstantTimeChat {
 	{
 		if(chatData == null)
 			return null;
-		
+
 		//packet len (4) | seedlen (4) ->0 | Magic (8) | Data | Padding
 		byte[] packetlenBytes = ByteBuffer.allocate(Integer.BYTES).putInt(ENV.FIXED_PACKET_SIZE_BIN).array();
 		byte[] seedLenBytes = ByteBuffer.allocate(Integer.BYTES).putInt(0x00).array();
-		
+
 		byte[] toSendWOpadding = new byte[8 + ENV.CHAT_MAGIC_BYTES_LEN + chatData.length];
-		
+
 		byte[] magicBytes = new byte[ENV.CHAT_MAGIC_BYTES_LEN];
 		Arrays.fill(magicBytes, ENV.CHAT_MAGIC_BYTES);
 		int tillNow = 0;
@@ -263,18 +301,18 @@ public class ResponseUtilBinConstantTimeChat {
 		tillNow += magicBytes.length;
 		System.arraycopy(chatData, 0, toSendWOpadding, tillNow, chatData.length);
 		tillNow += chatData.length;
-		
+
 		byte[] padding = new byte[ENV.FIXED_PACKET_SIZE_BIN - toSendWOpadding.length];
-		
+
 		if(ENV.RANDOM_PADDING)
 			rand.nextBytes(padding);
 		else
 			Arrays.fill(padding, ENV.PADDING_DETERMINISTIC_BYTE);
-		
+
 		byte[] toSend = new byte[ENV.FIXED_PACKET_SIZE_BIN];
 		System.arraycopy(toSendWOpadding, 0, toSend, 0, toSendWOpadding.length);
 		System.arraycopy(padding, 0, toSend, toSendWOpadding.length, padding.length);
-		
+
 		return toSend;
 	}
 }
